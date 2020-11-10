59d912676231792eb933cbf1d3b8c18fea1f228b
b1mind <brent@1mindsolutions.com>
2020-11-10 11:56:09 -0700

    🎹 Midi Piano

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,1 @@
-import './app.js'
-
-const nav = document.querySelector('.navMain')
-const canHazBurger = document.querySelector('.canHazBurger')
-
-canHazBurger.addEventListener('click', e => {
-	nav.classList.toggle('hidden')
-})
+import './piano.js'
