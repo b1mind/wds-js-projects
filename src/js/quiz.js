43c06c699f1e8cd888042e4a54bd14bb46142f9f
b1mind <brent@1mindsolutions.com>
2020-11-10 19:11:31 -0700

    🧙‍♂️quiz self challange complete

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,35 +1,37 @@
-/*
-  TODO: 2. Select all elements needed
-    * The form element (has the id `quiz-form`)
-    * The answer inputs (have the class `answer`)
-    * BONUS: The questions (have the class `question-item`)
-    * BONUS: The alert (has the id `alert`)
-*/
-
 const form = document.getElementById('quiz-form')
 const inputs = document.querySelectorAll('.answer')
 const questions = document.querySelectorAll('.question-item')
 const alert = document.getElementById('alert')
 
 function checkAnswers() {
+  let correctAnswers = 0
   inputs.forEach(e => {
     if (e.checked) {
       let parent = e.closest('.question-item')
       if (e.value === 'true') {
         parent.classList.add('correct')
         parent.classList.remove('incorrect')
+        correctAnswers++
       } else {
         parent.classList.remove('correct')
         parent.classList.add('incorrect')
+        correctAnswers--
       }
     }
   })
+
+  if (correctAnswers === 3) {
+    alert.classList.add('active')
+    setTimeout(() => alert.classList.remove('active'), 1500)
+  }
 }
 
 form.addEventListener('submit', e => {
   e.preventDefault()
+
   questions.forEach(e => {
     e.classList.add('incorrect')
   })
+
   checkAnswers()
 })
