7f1b243a1fcee3308e2dae9b32039e09322f68c6
b1mind <brent@1mindsolutions.com>
2020-11-10 18:29:27 -0700

    😡quiz self start

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -6,11 +6,26 @@
     * BONUS: The alert (has the id `alert`)
 */
 
-// TODO: 3. Create a submit event listener for the form that does the following.
-//    1. Prevent the default behaviour
-//    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
-//    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
-//    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
-//    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
-//    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
-//    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
+const form = document.getElementById('quiz-form')
+const inputs = document.querySelectorAll('.answer')
+const questions = document.querySelectorAll('.question-item')
+const alert = document.getElementById('alert')
+
+function checkAnswers() {
+  inputs.forEach(e => {
+    if (e.checked) {
+      if (e.value === 'true') {
+        console.log('correct')
+        console.dir(e)
+      } else {
+        console.log('incorrect')
+        console.dir(e)
+      }
+    }
+  })
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  checkAnswers()
+})
