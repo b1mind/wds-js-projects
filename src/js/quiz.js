0dd0833b17dcf39658d10213d62b03a3522f6c79
b1mind <brent@1mindsolutions.com>
2020-11-11 21:19:14 -0700

    ❓ updated with wds solutions

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,26 +1,40 @@
 const quizForm = document.getElementById('quiz-form')
-const answers = document.querySelectorAll('.answer')
+const answers = Array.from(document.querySelectorAll('.answer')) //kyle changed to array from node.list to expose filter
 const questions = document.querySelectorAll('.question-item')
 const congratsAlert = document.getElementById('alert')
 
 function checkAnswers() {
+  // kyle used filter to get an array of all selected answers
+  let selectedAnswers = answers.filter(answer => answer.checked)
+
+  // where I was trying to hold count
   let correctAnswers = 0
-  answers.forEach(e => {
-    if (e.checked) {
-      let parent = e.closest('.question-item')
-      if (e.value === 'true') {
+
+  answers.forEach(answer => {
+    if (answer.checked) {
+      let parent = answer.closest('.question-item')
+
+      if (answer.value === 'true') {
         parent.classList.add('correct')
         parent.classList.remove('incorrect')
+
         correctAnswers++
       } else {
         parent.classList.remove('correct')
         parent.classList.add('incorrect')
+
         correctAnswers--
       }
     }
   })
 
-  if (correctAnswers === 3) {
+  // kyle was then able to also check every answer for value true
+  let allTrue = selectedAnswers.every(answer => answer.value === 'true')
+  // then checked if all answers matched number of questions
+  let allAnswered = selectedAnswers === questions.length
+
+  // now you have two booleans to check against (kept my solution too)
+  if (correctAnswers === 3 || (allTrue && allAnswered)) {
     congratsAlert.classList.add('active')
     setTimeout(() => congratsAlert.classList.remove('active'), 1500)
   }
