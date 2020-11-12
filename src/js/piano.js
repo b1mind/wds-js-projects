56eaab420ce00b2d74a19ef6018c663008e3855a
b1mind <brent@1mindsolutions.com>
2020-11-12 10:43:17 -0700

    🎹 Moved keys

--- src/js/piano.js
+++ src/js/piano.js
@@ -10,17 +10,18 @@ canHazBurger.addEventListener('click', e => {
 })
 
 const NOTE_DETAILS = [
-  { note: 'C', key: 'Z', frequency: 261.626, active: false },
-  { note: 'Db', key: 'S', frequency: 277.183, active: false },
-  { note: 'D', key: 'X', frequency: 293.665, active: false },
-  { note: 'Eb', key: 'D', frequency: 311.127, active: false },
-  { note: 'E', key: 'C', frequency: 329.628, active: false },
-  { note: 'F', key: 'V', frequency: 349.228, active: false },
-  { note: 'Gb', key: 'G', frequency: 369.994, active: false },
-  { note: 'Ab', key: 'H', frequency: 391.995, active: false },
-  { note: 'A', key: 'N', frequency: 440, active: false },
-  { note: 'Bb', key: 'J', frequency: 466.164, active: false },
-  { note: 'B', key: 'M', frequency: 493.883, active: false },
+  { note: 'C', key: 'S', frequency: 261.626, active: false },
+  { note: 'Db', key: 'E', frequency: 277.183, active: false },
+  { note: 'D', key: 'D', frequency: 293.665, active: false },
+  { note: 'Eb', key: 'R', frequency: 311.127, active: false },
+  { note: 'E', key: 'F', frequency: 329.628, active: false },
+  { note: 'F', key: 'H', frequency: 349.228, active: false },
+  { note: 'Gb', key: 'U', frequency: 369.994, active: false },
+  { note: 'G', key: 'J', frequency: 369.994, active: false },
+  { note: 'Ab', key: 'I', frequency: 391.995, active: false },
+  { note: 'A', key: 'K', frequency: 440, active: false },
+  { note: 'Bb', key: 'O', frequency: 466.164, active: false },
+  { note: 'B', key: 'L', frequency: 493.883, active: false },
 ]
 
 document.addEventListener('keydown', e => {
