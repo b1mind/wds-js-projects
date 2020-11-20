3df9e92bdbce1f62ca4793cbc1b9614c4c12edb7
b1mind <brent@1mindsolutions.com>
2020-11-20 00:30:27 -0700

    🧙‍♂️accordion complete

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,1 +1,19 @@
 console.log('lets be challenged to make a accordion')
+
+// make a e.delegation for buttons
+const accordion = document.querySelector('#accordion')
+accordion.addEventListener('click', e => {
+  const btn = e.target.closest('button')
+  if (!btn) return
+
+  const parent = btn.closest('.card')
+  const cardBody = parent.querySelector('.card-body')
+  cardBody.classList.toggle('show')
+
+  // check if show class if not collapse
+  if (cardBody.classList.contains('show')) {
+    btn.innerText = 'Collapse'
+  } else {
+    btn.innerText = 'Expand'
+  }
+})
